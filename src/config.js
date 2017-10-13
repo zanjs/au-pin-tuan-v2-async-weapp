54d2641aa8201331b0d64f4466aaabe0368af893
zanjs <root@zanjs.com>
2017-10-13 14:23:54 +0800

    是

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import Version from './version'
 module.exports = {
   Version,
   LogWrite: true,
-  // ApiHost: 'https://au.anla.io/',
-  ApiHost: 'http://au.de/',
+  ApiHost: 'https://au.anla.io/',
+  // ApiHost: 'http://au.de/',
   FileHost: 'http://img.au.anla.io/storage/',
   // ApiHost: 'http://192.168.1.184:8000/',
   // FileHost: 'http://192.168.1.184:8000',
